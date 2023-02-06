eb706180d6a379008dbdf29a53a92f5f1fb9f0f3
Luis Arturo <luisdemaryori@gmail.com>
2023-02-06 14:49:06 -0500

    Primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,49 @@
+const { Usuarios } = require("../classes/usuarios");
+const { io } = require("../server");
+const { crearMensaje } = require("../utilidades/utilidades");
+const usuarios = new Usuarios();
+io.on("connection", (client) => {
+  client.on("entrarChat", (data, callback) => {
+    console.log(data);
+    if (!data.nombre || !data.sala) {
+      return callback({
+        error: true,
+        mensaje: "El nombre/sala es necesario",
+      });
+    }
+
+    client.join(data.sala); //unir al cliente en una sala en particular
+
+    usuarios.agregarPersona(client.id, data.nombre, data.sala);
+    client.broadcast
+      .to(data.sala)
+      .emit("listaPersona", usuarios.getPersonasPorSala(data.sala));
+     callback(usuarios.getPersonasPorSala(data.sala));
+  });
+  client.on("crearMensaje", (data) => {
+    let persona = usuarios.getPersona(client.id);
+    let mensaje = crearMensaje(persona.nombre, data.mensaje);
+    client.broadcast.to(persona.sala).emit("crearMensaje", mensaje);
+  });
+
+  client.on("disconnect", () => {
+    let personaBorrada = usuarios.borrarPersona(client.id); //arregla la duplicidad de los usuarios
+    client.broadcast
+      .to(personaBorrada.sala)
+      .emit(
+        "crearMensaje",
+        crearMensaje("Administrador", `${personaBorrada.nombre} salio`)
+      );
+    client.broadcast
+      .to(personaBorrada.sala)
+      .emit("listaPersona", usuarios.getPersonasPorSala(personaBorrada.sala));
+  });
+
+  //mensajes privado
+  client.on("mensajePrivado", (data) => {
+    let persona = usuarios.getPersona(client.id);
+    client.broadcast
+      .to(data.para)
+      .emit("mensajePrivado", crearMensaje(persona.nombre, data.mensaje));
+  });
+});
