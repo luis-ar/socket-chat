af7d8d3c8736b99a6924746cf0a4caa63d22f6f7
Luis Arturo <luisdemaryori@gmail.com>
2023-02-06 16:52:47 -0500

    chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,11 +1,13 @@
-const socket = io();
-const params = new URLSearchParams(window.location.search);
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
 
 if (!params.has("nombre") || !params.has("sala")) {
   window.location = "index.html";
   throw new Error("El nombre y sala son necesarios");
 }
-const usuario = {
+
+var usuario = {
   nombre: params.get("nombre"),
   sala: params.get("sala"),
 };
@@ -14,7 +16,8 @@ socket.on("connect", function () {
   console.log("Conectado al servidor");
 
   socket.emit("entrarChat", usuario, function (resp) {
-    console.log("usuarios conectados ", resp);
+    console.log("Usuarios conectados", resp);
+    renderizarUsuarios(resp)
   });
 });
 
@@ -24,29 +27,28 @@ socket.on("disconnect", function () {
 });
 
 // Enviar información
-// socket.emit(
-//   "crearMensaje",
-//   {
-//     usuario: "Fernando",
-//     mensaje: "Hola Mundo",
-//   },
-//   function (resp) {
-//     console.log("respuesta server: ", resp);
-//   }
-// );
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on("crearMensaje", function (mensaje) {
   console.log("Servidor:", mensaje);
+  renderizarMensajes(mensaje,false)
+  scrollBottom()
 });
-//escuchar cambios de usuarios
-//cuando un usuario entra o sale del chat
+
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on("listaPersona", function (personas) {
   console.log(personas);
+  renderizarUsuarios(personas)
 });
 
-//mensajes privados
-
+// Mensajes privados
 socket.on("mensajePrivado", function (mensaje) {
-  console.log("Mensaje privado: ", mensaje);
+  console.log("Mensaje Privado:", mensaje);
 });
